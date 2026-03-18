92c0199d215f231e787be79ebc990dd088529531
Danylo <pechatkin.work@gmail.com>
2026-03-18 01:17:30 -0400

    analytics added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from "@vercel/analytics/react";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
@@ -134,6 +135,7 @@ export default function RootLayout({
         </header>
 
         {children}
+         <Analytics />
       </body>
     </html>
   );
