1819c3168e709acedec91aaae034c850ebbb278c
Danylo <103935021+danylopechatkin@users.noreply.github.com>
2026-05-18 07:27:04 +0200

    Update layout.tsx

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -9,6 +9,7 @@ import { Analytics } from "@vercel/analytics/react";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "../globals.css";
+import Script from "next/script";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
